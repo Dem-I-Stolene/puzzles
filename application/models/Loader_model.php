84dfbf1d51a8a4434fc4d69fb4de91de0c813747
Voss19 <silvermaster13@gmail.com>
2018-04-12 13:29:38 +0200

    html

--- application/models/Loader_model.php
+++ application/models/Loader_model.php
@@ -23,7 +23,21 @@ class Loader_model extends CI_Model {
 		if (!$content) {
 			$content = 'page_not_found';
 		}
-		
+
+		// General variables
+		$data['title'] = (isset($data['title']))? $data['title'] : 'Puzzle';
+		$data['logo'] = base_url('assets/images/logo.png');
+		$data['css'][]['style'] = 'main';
+
+		// Fix css names
+		foreach ($data['css'] as $key => $value) {
+			$data['css'][$key]['style'] = base_url('assets/css/').$value['style'].'.css?v='.rand(1, 100);
+		}
+
+		// Load page
+		$this->parser->parse('template/head', $data);
+		$this->parser->parse('template/nav', $data);
 		$this->parser->parse($content, $data);
+		$this->parser->parse('template/end', $data);
 	}
 }
---
+++ application/views/template/nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg">
+  <div class="container">
+  <img src="{logo}" id="nav-logo">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#">Home</a>
+    </div>
+  </div>
+</div>
+</nav>
+<div class="container">
